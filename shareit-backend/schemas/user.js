24e0e5deaa663fa29418aa58b05735e72e223b0f
tommi <aritonang31031996@gmail.com>
2022-11-17 22:05:43 +0700

    menambah sanity

---
+++ shareit-backend/schemas/user.js
@@ -0,0 +1,19 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'username',
+            title: 'UserName',
+            type: 'string'
+        },
+
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string'
+        },
+    ]
+
+}
